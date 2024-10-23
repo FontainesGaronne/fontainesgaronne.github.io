672461df397a95d8bca2912fe4fa8f415a242e0e
Florian Sommariva <1926041+dtrucs@users.noreply.github.com>
2024-10-23 14:38:40 +0200

    Add sitemap.xml

---
+++ app/sitemap.ts
@@ -0,0 +1,33 @@
+import client from "../tina/__generated__/client";
+
+export default async function sitemap() {
+  const [pages, posts, agenda] = await Promise.all([
+    client.queries.pageConnection(),
+    client.queries.postConnection(),
+    client.queries.agendaConnection()
+  ]);
+  const links = [];
+  if ((pages.data?.pageConnection.edges ?? []).length > 0) {
+    links.push(...pages.data.pageConnection.edges.map(
+      (edge) => {
+        const breadcrumbs = edge.node._sys.breadcrumbs.join("/");
+        return { url: `/` + breadcrumbs !== "home" ? breadcrumbs : "" }
+      }
+    ));
+  }
+  if ((posts.data?.postConnection.edges ?? []).length > 0) {
+    links.push(...posts.data.postConnection.edges.map(
+      (edge) => (
+        { url: `/post/` + edge.node._sys.breadcrumbs.join("/") }
+      )
+    ));
+  }
+  if ((agenda.data?.agendaConnection.edges ?? []).length > 0) {
+    links.push(...agenda.data.agendaConnection.edges.map(
+      (edge) => (
+        { url: `/agenda/` + edge.node._sys.breadcrumbs.join("/") }
+      )
+    ));
+  }
+  return links;
+}
