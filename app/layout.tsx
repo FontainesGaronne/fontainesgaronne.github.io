247bd6b5e66dc314c79e72e8c4f18d3dd9d9fb78
Florian Sommariva <1926041+dtrucs@users.noreply.github.com>
2025-02-20 17:44:48 +0100

    Define favicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -61,6 +61,12 @@ export default async function RootLayout({
         {/* if you aren't deploying to github pages, feel free to delete these tags */}
         <meta name="X-Frame-Options" content="SAMEORIGIN" />
         <meta name="Content-Security-Policy" content="frame-ancestors 'self'" />
+        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <meta name="apple-mobile-web-app-title" content="CQFG" />
+        <link rel="manifest" href="/site.webmanifest" />
       </head>
       <body
         suppressHydrationWarning
