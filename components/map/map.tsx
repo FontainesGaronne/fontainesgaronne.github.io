c2b13ed7c89187e2b4e4310714526da96db0f65b
Florian Sommariva <1926041+dtrucs@users.noreply.github.com>
2025-03-16 18:12:00 +0100

    Create Map component

---
+++ components/map/map.tsx
@@ -0,0 +1,54 @@
+import * as React from 'react';
+import MapLibre, { FullscreenControl, Layer, MapRef, NavigationControl, ScaleControl, Source } from 'react-map-gl/maplibre';
+import 'maplibre-gl/dist/maplibre-gl.css';
+import { districtGeojson, toulouseGeojson } from '@/components/map/style';
+import { getBoundsFromCoordinates } from '@/components/map/utils';
+import bbox from '@turf/bbox';
+
+export default function Map() {
+  const mapRef = React.useRef<MapRef>(null);
+
+  const bounds = getBoundsFromCoordinates(bbox(districtGeojson.features[0]))
+  const maxBounds = getBoundsFromCoordinates(bbox(toulouseGeojson.features[0]));
+
+  const handleClick = () => {
+    mapRef.current.fitBounds(bounds, {padding: 40, duration: 1000});
+  };
+
+  return (
+    <MapLibre
+      ref={mapRef}
+      initialViewState={{
+        bounds,
+      }}
+      maxBounds={maxBounds}
+      mapStyle="https://tiles.openfreemap.org/styles/liberty"
+      interactiveLayerIds={['CQFG-district-fill']}
+      onClick={handleClick}
+    >
+      <Source id="CQFG-district" type="geojson" data={districtGeojson}>
+      <Layer  
+          id="CQFG-district-fill"
+          type="fill"
+          paint={{
+            'fill-outline-color': 'rgb(234, 179, 8)',
+            'fill-color': '#fff',
+            'fill-opacity': 0
+          }}
+        />
+        <Layer  
+          id="CQFG-district-outline"
+          type="line"
+          paint={{
+            'line-width': 20,
+            'line-color': 'rgb(234, 179, 8)',
+            "line-blur": 16
+          }}
+        />
+      </Source>
+      <NavigationControl />
+      <FullscreenControl />
+      <ScaleControl />
+    </MapLibre>
+  );
+}
