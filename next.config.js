b089f39c65405b5425e4648ce0a8f574dad9b956
Florian Sommariva <1926041+dtrucs@users.noreply.github.com>
2025-04-04 22:16:51 +0200

    Change build export out

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  output: "export",
   images: {
     unoptimized: true,
     remotePatterns: [
@@ -17,32 +18,5 @@ module.exports = {
     });
 
     return config;
-  },
-  async headers() {
-    // these are also defined in the root layout since github pages doesn't support headers
-    const headers = [
-      {
-        key: "X-Frame-Options",
-        value: "SAMEORIGIN",
-      },
-      {
-        key: "Content-Security-Policy",
-        value: "frame-ancestors 'self'",
-      },
-    ];
-    return [
-      {
-        source: "/(.*)",
-        headers,
-      },
-    ];
-  },
-  async rewrites() {
-    return [
-      {
-        source: "/admin",
-        destination: "/admin/index.html",
-      },
-    ];
   },
 };
