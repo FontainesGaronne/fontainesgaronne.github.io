e40a4f6e92173f200b4415963b27f78b4bef0ce3
Florian Sommariva <1926041+dtrucs@users.noreply.github.com>
2026-02-07 23:08:34 +0100

    Define permanently redirections

---
+++ _redirects.mjs
@@ -0,0 +1,163 @@
+// From wordpress to astro 🚀
+export default {
+  "/2nde-edition-halles-de-laventure-cartoucherie-vendredi-5-octobre-19h":
+    "/posts/2nde-edition-halles-de-laventure-cartoucherie-vendredi-5-octobre-19h",
+  "/308": "/posts/308",
+  "/331": "/posts/331",
+  "/645-2": "/posts/645-2",
+  "/748-2": "/posts/748-2",
+  "/93": "/posts/93",
+  "/agenda-des-evenements-davril-a-juillet":
+    "/posts/agenda-des-evenements-davril-a-juillet",
+  "/amis-de-piscine-ancely-ne-plongent":
+    "/posts/amis-de-piscine-ancely-ne-plongent",
+  "/animations-nature-climat-dimanche-14-mai":
+    "/posts/animations-nature-climat-dimanche-14-mai",
+  "/animations-nature-climat-le-dimanche-5-mai-2024":
+    "/posts/animations-nature-climat-le-dimanche-5-mai-2024",
+  "/animations-nature-climat-le-dimanche-8-octobre":
+    "/posts/animations-nature-climat-le-dimanche-8-octobre",
+  "/appel-a-contribution-dabricoop": "/posts/appel-a-contribution-dabricoop",
+  "/assemblee-generale-comite-de-quartier-lundi-28-janvier-2019-a-21h00-place-baudin":
+    "/posts/assemblee-generale-comite-de-quartier-lundi-28-janvier-2019-a-21h00-place-baudin",
+  "/atelier-velo-organise-cercle-laique":
+    "/posts/atelier-velo-organise-cercle-laique",
+  "/atelier-velo-participatif-dimanche-marche-job":
+    "/posts/atelier-velo-participatif-dimanche-marche-job",
+  "/a-vos-agendas": "/posts/a-vos-agendas",
+  "/bonjour-tout-le-monde": "/posts/bonjour-tout-le-monde",
+  "/cafe-salon-de-the-magnolia-accueille-ateliers-creatifs":
+    "/posts/cafe-salon-de-the-magnolia-accueille-ateliers-creatifs",
+  "/carnaval-de-quartier-8-avril-2016":
+    "/posts/carnaval-de-quartier-8-avril-2016",
+  "/carnaval-enfants-8-avril": "/posts/carnaval-enfants-8-avril",
+  "/cercle-laique-casselardit-rentree":
+    "/posts/cercle-laique-casselardit-rentree",
+  "/chalets-prets-a-ceder-piscine-ancely-a-mairie-leuro-symbolique":
+    "/posts/chalets-prets-a-ceder-piscine-ancely-a-mairie-leuro-symbolique",
+  "/cinema-de-plein-air-samedi-3-septembre-carrefour-arenes-place-emile-male":
+    "/posts/cinema-de-plein-air-samedi-3-septembre-carrefour-arenes-place-emile-male",
+  "/cinema-en-plein-air-le-samedi-3-juin":
+    "/posts/cinema-en-plein-air-le-samedi-3-juin",
+  "/cinema-plein-air-aux-arenes-romaines-1er-juillet":
+    "/posts/cinema-plein-air-aux-arenes-romaines-1er-juillet",
+  "/cine-plein-air-27-mai-place-baudin":
+    "/posts/cine-plein-air-27-mai-place-baudin",
+  "/cine-plein-air-place-baudin-17-septembre":
+    "/posts/cine-plein-air-place-baudin-17-septembre",
+  "/commission-de-quartier-16-novembre":
+    "/posts/commission-de-quartier-16-novembre",
+  "/commission-de-quartier-28-juin-18h30-salle-barry":
+    "/posts/commission-de-quartier-28-juin-18h30-salle-barry",
+  "/composteur-collectif-parc-fontaines":
+    "/posts/composteur-collectif-parc-fontaines",
+  "/compte-rendu-ag-tenue-30-janvier-2017":
+    "/posts/compte-rendu-ag-tenue-30-janvier-2017",
+  "/concert-marche-artisanal-animations-enfants-parc-pouvillon-20-mai":
+    "/posts/concert-marche-artisanal-animations-enfants-parc-pouvillon-20-mai",
+  "/coronavirus-message-entraide": "/posts/coronavirus-message-entraide",
+  "/courriers-mairie": "/posts/courriers-mairie",
+  "/dates-a-retenir-2": "/posts/dates-a-retenir-2",
+  "/debat-public-ligne-3-metro": "/posts/debat-public-ligne-3-metro",
+  "/document-du-ccnaat": "/posts/document-du-ccnaat",
+  "/evenements-organises-lassociation-ti-kaz-ami":
+    "/posts/evenements-organises-lassociation-ti-kaz-ami",
+  "/festival-de-photo-map-halles-de-cartoucherie":
+    "/posts/festival-de-photo-map-halles-de-cartoucherie",
+  "/fete-a-cartoucherie-jeudi-14-week-end-16-17-decembre":
+    "/posts/fete-a-cartoucherie-jeudi-14-week-end-16-17-decembre",
+  "/fete-de-lumiere-a-cartoucherie-13-decembre-a-17h":
+    "/posts/fete-de-lumiere-a-cartoucherie-13-decembre-a-17h",
+  "/fete-des-couleurs-a-la-cartoucherie":
+    "/posts/fete-des-couleurs-a-la-cartoucherie",
+  "/forum-associations-samedi-17-septembre-de-15h-a-19h":
+    "/posts/forum-associations-samedi-17-septembre-de-15h-a-19h",
+  "/info-mairie-commision-de-quartier-14-fevrier-a-18h30-salle-barry":
+    "/posts/info-mairie-commision-de-quartier-14-fevrier-a-18h30-salle-barry",
+  "/jardin-partage-place-micoucouliers-ouvert":
+    "/posts/jardin-partage-place-micoucouliers-ouvert",
+  "/jardin-partage-quartier": "/posts/jardin-partage-quartier",
+  "/la-fete-de-la-musique-du-cercle-laique-dimanche-10-juin":
+    "/posts/la-fete-de-la-musique-du-cercle-laique-dimanche-10-juin",
+  "/legumes-bio-quartier": "/posts/legumes-bio-quartier",
+  "/message-de-nos-voisins-quartier-ancely":
+    "/posts/message-de-nos-voisins-quartier-ancely",
+  "/mobilisation-piscine-ancely-acte-iii":
+    "/posts/mobilisation-piscine-ancely-acte-iii",
+  "/newsletter-numero-10-disponible": "/posts/newsletter-numero-10-disponible",
+  "/newsletter-numero-7-disponible": "/posts/newsletter-numero-7-disponible",
+  "/nouveau-lieu-de-restauration-de-convivialite-a-cartoucherie":
+    "/posts/nouveau-lieu-de-restauration-de-convivialite-a-cartoucherie",
+  "/nouvelle-adresse-lentretien-de-velo":
+    "/posts/nouvelle-adresse-lentretien-de-velo",
+  "/ouverture-cafe-creatif-magnolia-208-avenue-de-grande-bretagne":
+    "/posts/ouverture-cafe-creatif-magnolia-208-avenue-de-grande-bretagne",
+  "/piscine-ancely-projet-faire-renaitre":
+    "/posts/piscine-ancely-projet-faire-renaitre",
+  "/projet-de-composteur-collectif-jardin-fontaines":
+    "/posts/projet-de-composteur-collectif-jardin-fontaines",
+  "/quartier-entier-passe-zone-30": "/posts/quartier-entier-passe-zone-30",
+  "/quelques-nouvelles-halles-de-cartoucherie":
+    "/posts/quelques-nouvelles-halles-de-cartoucherie",
+  "/rampe-dacces-a-digue-de-garonne-ca-bouge":
+    "/posts/rampe-dacces-a-digue-de-garonne-ca-bouge",
+  "/reamenagement-boulevard-jean-brunhes":
+    "/posts/reamenagement-boulevard-jean-brunhes",
+  "/rencontre-habitants-comite-de-quartier-mardi-21-novembre-de-17h-a-20h":
+    "/posts/rencontre-habitants-comite-de-quartier-mardi-21-novembre-de-17h-a-20h",
+  "/rencontre-habitants-comite-de-quartier-mardi-27-mars-de-17h-a-20h-salle-de-basket-place-baudin":
+    "/posts/rencontre-habitants-comite-de-quartier-mardi-27-mars-de-17h-a-20h-salle-de-basket-place-baudin",
+  "/rencontre-police-population-quartier-fontaine-bayonne-cartoucherie":
+    "/posts/rencontre-police-population-quartier-fontaine-bayonne-cartoucherie",
+  "/rendez-a-ne-manquer": "/posts/rendez-a-ne-manquer",
+  "/repas-de-quartier-13-juin-place-baudin-a-partir-de-19h":
+    "/posts/repas-de-quartier-13-juin-place-baudin-a-partir-de-19h",
+  "/repas-de-quartier-14-juin-place-baudin-a-partir-de-19h":
+    "/posts/repas-de-quartier-14-juin-place-baudin-a-partir-de-19h",
+  "/repas-de-quartier-3-juin": "/posts/repas-de-quartier-3-juin",
+  "/repas-de-quartier-le-vendredi-7-juin--partir-de-19h":
+    "/posts/repas-de-quartier-le-vendredi-7-juin--partir-de-19h",
+  "/repas-de-quartier-vendredi-01-juin-place-baudin":
+    "/posts/repas-de-quartier-vendredi-01-juin-place-baudin",
+  "/repas-de-quartier-vendredi-2-juin":
+    "/posts/repas-de-quartier-vendredi-2-juin",
+  "/report-fete-de-musique-place-baudin-dimanche-23-juin":
+    "/posts/report-fete-de-musique-place-baudin-dimanche-23-juin",
+  "/reunion-concertation-place-baudin-04-decembre-2018":
+    "/posts/reunion-concertation-place-baudin-04-decembre-2018",
+  "/reunion-mardi-130916-19h30-amenagement-rues-murillo-gais-pinsons-charleville":
+    "/posts/reunion-mardi-130916-19h30-amenagement-rues-murillo-gais-pinsons-charleville",
+  "/reunions-a-venir-a-mairie-de-quartier-rues-sembat-lespes-talazac":
+    "/posts/reunions-a-venir-a-mairie-de-quartier-rues-sembat-lespes-talazac",
+  "/rue-fontaines-plus-agreable-plus-securisante":
+    "/posts/rue-fontaines-plus-agreable-plus-securisante",
+  "/rue-fontaines-plus-agreable-securisante-quen-pensez":
+    "/posts/rue-fontaines-plus-agreable-securisante-quen-pensez",
+  "/rue-fontaines-reponses-candidats-a-lelection-municipale-2020":
+    "/posts/rue-fontaines-reponses-candidats-a-lelection-municipale-2020",
+  "/rue-fontaines-reunion-de-concertation-mercredi-11-decembre-20h45":
+    "/posts/rue-fontaines-reunion-de-concertation-mercredi-11-decembre-20h45",
+  "/seul-bus-tisseo-irrigue-quartier-danger":
+    "/posts/seul-bus-tisseo-irrigue-quartier-danger",
+  "/signez-petition-maintien-66-dimanche-lamelioration-45":
+    "/posts/signez-petition-maintien-66-dimanche-lamelioration-45",
+  "/stop-aux-moustiques": "/posts/stop-aux-moustiques",
+  "/test": "/posts/test",
+  "/tour-de-france-solidaire-emmaus": "/posts/tour-de-france-solidaire-emmaus",
+  "/troc-de-plantes-12-juin-a-licam-de-10h-a-13h":
+    "/posts/troc-de-plantes-12-juin-a-licam-de-10h-a-13h",
+  "/troc-de-plantes-15-mai": "/posts/troc-de-plantes-15-mai",
+  "/troc-de-plantes-3-juin-de-10h-a-13h-place-baudin":
+    "/posts/troc-de-plantes-3-juin-de-10h-a-13h-place-baudin",
+  "/troc-de-plantes-plus-19-mai-place-baudin":
+    "/posts/troc-de-plantes-plus-19-mai-place-baudin",
+  "/troc-de-plantes-vide-grenier-23-avril-parking-de-licam":
+    "/posts/troc-de-plantes-vide-grenier-23-avril-parking-de-licam",
+  "/une-amap-dans-le-quartier": "/posts/une-amap-dans-le-quartier",
+  "/venez-feter-musique-place-baudin-21-juin":
+    "/posts/venez-feter-musique-place-baudin-21-juin",
+  "/venez-nombreux-repas-de-quartier-16-septembre-place-baudin":
+    "/posts/venez-nombreux-repas-de-quartier-16-septembre-place-baudin",
+  "/village-interculturel-place-emile-male-arenes-1er-avril":
+    "/posts/village-interculturel-place-emile-male-arenes-1er-avril",
+};
--- astro.config.mjs
+++ astro.config.mjs
@@ -5,14 +5,21 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import react from '@astrojs/react';
 import tinaDirective from "./astro-tina-directive/register"
+import redirects from "./_redirects.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-	site: process.env.SITE_URL || `https://fontainesgaronne.github.io/`,
-	integrations: [mdx(), sitemap(), react({
-		experimentalReactChildren: true,
-	}), tinaDirective()],
-	vite: {
+  site: process.env.SITE_URL || `https://fontainesgaronne.github.io/`,
+  integrations: [
+    mdx(),
+    sitemap(),
+    react({
+      experimentalReactChildren: true,
+    }),
+    tinaDirective(),
+  ],
+  vite: {
     plugins: [tailwindcss()],
-  },	
+  },
+  redirects,
 });
